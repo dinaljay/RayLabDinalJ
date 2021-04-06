7e47aa33494cf2fdb0380bbcb091ddbaede982df
dinaljay <dinal.ais@gmail.com>
2021-04-05 22:16:57 -0500

    Updates

---
+++ MATLAB_DBSI/Mat file creator scripts/Post_op/ROI_by_slice/volume_calc.m
@@ -0,0 +1,167 @@
+% This script calculates the volume and surface area of the selected ROI_by_slices
+
+clear all;
+close all;
+addpath (genpath('/home/functionalspinelab/Documents/MATLAB'));
+addpath (genpath('/home/functionalspinelab/Desktop/Dinal/Scripts/MATLAB_DBSI'));
+
+%% File paths
+
+control_path = '/media/functionalspinelab/RAID/Data/Dinal/DBSI_Data/CSM_New/Control';
+csm_path = '/media/functionalspinelab/RAID/Data/Dinal/DBSI_Data/CSM_New/Patient';
+out_dir_control = '/media/functionalspinelab/RAID/Data/Dinal/MATLAB_Data/DBSI/Post_op/ROI_by_slice/Control';
+out_dir_mild_csm = '/media/functionalspinelab/RAID/Data/Dinal/MATLAB_Data/DBSI/Post_op/ROI_by_slice/Mild_CSM';
+out_dir_mod_csm = '/media/functionalspinelab/RAID/Data/Dinal/MATLAB_Data/DBSI/Post_op/ROI_by_slice/Moderate_CSM';
+
+%% Declare necessary variables
+
+controls = [4,5,8,9,10,11,15,16,17,18];
+
+mild_cm_subjects = [2,3,5,15,18,19,23,28,29,36,40];
+% waiting on 16
+
+moderate_cm_subjects = [9,12,20,27];
+
+cm_subjects = [mild_cm_subjects,moderate_cm_subjects];
+
+cm_subjects = sort(cm_subjects,2);
+
+slices = (1:1:4);
+
+voxel_volume = 0.35*0.35*7.5;
+
+voxel_surface_area = 0.35*0.35;
+
+%% Load patient data for each condition
+
+fprintf('Control Patients \n')
+
+data_control = cell(numel(controls),1);
+
+for k = 1:numel(controls)
+    
+    subjectID = strcat('CSM_C0',num2str(controls(k)));
+    disp(num2str(subjectID));
+    temp = [];
+    
+    for j = 1:numel(slices)
+        
+        slice_num = strcat('slice_',num2str(slices(j)));
+        disp(num2str(slice_num));
+        
+        param_file =('dti_fa_map.nii');
+        mask_file = fullfile(control_path,subjectID,'/scan_2/dMRI_ZOOMit/',slice_num,'/all_volumes/label/template/PAM50_wm.nii.gz');
+        dwi_file = fullfile(control_path,subjectID,'/scan_2/dMRI_ZOOMit/',slice_num,'/all_volumes/DHI_results_0.3_0.3_3_3',param_file);
+        
+        mask = niftiread(mask_file);
+        dwi_data = niftiread(dwi_file);
+        
+        expert_rois = double(mask);
+        dwi_data = double(dwi_data);
+        
+        data = dwi_data(expert_rois>0.7);
+        data_control{k,j} = length(data);
+    end
+end
+
+fprintf('\n')
+fprintf('Mild Cervical Myelopathy Patients \n')
+
+data_mild_csm = cell(numel(mild_cm_subjects),1);
+
+for k = 1:numel(mild_cm_subjects)
+    
+    subjectID = strcat('CSM_P0',num2str(mild_cm_subjects(k)));
+    disp(num2str(subjectID));
+    temp = [];
+    
+    for j = 1:numel(slices)
+        slice_num = strcat('slice_',num2str(slices(j)));
+        disp(num2str(slice_num));
+        
+        param_file =('dti_fa_map.nii');
+        mask_file = fullfile(csm_path,subjectID,'/scan_2/dMRI_ZOOMit/',slice_num,'/all_volumes/label/template/PAM50_wm.nii.gz');
+        dwi_file = fullfile(csm_path,subjectID,'/scan_2/dMRI_ZOOMit/',slice_num,'/all_volumes/DHI_results_0.3_0.3_3_3',param_file);
+        
+        mask = niftiread(mask_file);
+        dwi_data = niftiread(dwi_file);
+        
+        expert_rois = double(mask);
+        dwi_data = double(dwi_data);
+        data = dwi_data(expert_rois>0.7);
+        data_mild_csm{k,j} = length(data);
+    end
+end
+
+fprintf('\n')
+fprintf('Moderate Cervical Myelopathy Patients \n')
+
+data_mod_csm = cell(numel(moderate_cm_subjects),1);
+
+for k = 1:numel(moderate_cm_subjects)
+    
+    subjectID = strcat('CSM_P0',num2str(moderate_cm_subjects(k)));
+    disp(num2str(subjectID));
+    temp = [];
+    
+    for j = 1:numel(slices)
+        slice_num = strcat('slice_',num2str(slices(j)));
+        disp(num2str(slice_num));
+        
+        param_file =('dti_fa_map.nii');
+        mask_file = fullfile(csm_path,subjectID,'/scan_2/dMRI_ZOOMit/',slice_num,'/all_volumes/label/template/PAM50_wm.nii.gz');
+        dwi_file = fullfile(csm_path,subjectID,'/scan_2/dMRI_ZOOMit/',slice_num,'/all_volumes/DHI_results_0.3_0.3_3_3',param_file);
+        
+        mask = niftiread(mask_file);
+        dwi_data = niftiread(dwi_file);
+        
+        expert_rois = double(mask);
+        dwi_data = double(dwi_data);
+        data = dwi_data(expert_rois>0.7);
+        data_mod_csm{k,j} = length(data);
+    end
+end
+
+%% Caculate volume and surface area
+
+% Controls
+for i = 1:length(data_control)
+    for j = 1:numel(slices)
+        control_volumes{i,j} = data_control{i,j}*voxel_volume;
+        control_surface_area{i,j} = data_control{i,j}*voxel_surface_area;
+    end
+end
+
+terminal = strcat('control_volume','_data.mat');
+save(fullfile(out_dir_control,terminal),'controls','control_volumes');
+
+terminal = strcat('control_surface_area','_data.mat');
+save(fullfile(out_dir_control,terminal),'controls','control_surface_area');
+
+% Mild CM subjects
+for i = 1:length(data_mild_csm)
+    for j = 1:numel(slices)
+        mild_csm_volumes{i,j} = data_mild_csm{i,j}*voxel_volume;
+        mild_csm_surface_area{i,j} = data_mild_csm{i,j}*voxel_surface_area;
+    end
+end
+
+terminal = strcat('mild_csm_volume','_data.mat');
+save(fullfile(out_dir_mild_csm,terminal),'mild_cm_subjects','mild_csm_volumes');
+
+terminal = strcat('mild_csm_surface_area','_data.mat');
+save(fullfile(out_dir_mild_csm,terminal),'mild_cm_subjects','mild_csm_surface_area');
+
+% Moderate CM subjects
+for i = 1:length(data_mod_csm)
+    for j = 1:numel(slices)
+        mod_csm_volumes{i,j} = data_mod_csm{i,j}*voxel_volume;
+        mod_csm_surface_area{i,j} = data_mod_csm{i,j}*voxel_surface_area;
+    end
+end
+
+terminal = strcat('mod_csm_volume','_data.mat');
+save(fullfile(out_dir_mod_csm,terminal),'moderate_cm_subjects','mod_csm_volumes');
+
+terminal = strcat('mod_csm_surface_area','_data.mat');
+save(fullfile(out_dir_mod_csm,terminal),'moderate_cm_subjects','mod_csm_surface_area');
---
+++ Python_scripts/Pre_op/Plots/boxplot_pre_and_post_by_slice.py
@@ -0,0 +1,66 @@
+
+import numpy as np
+import os.path as path
+import matplotlib.pyplot as plt
+import pandas as pd
+import sys
+import seaborn as sns
+
+## Load Data
+
+url1 = '/media/functionalspinelab/RAID/Data/Dinal/Pycharm_Data_ROI_by_slice/all_ROI_by_slice_1_data.csv'
+slice1_data = pd.read_csv(url1)
+
+url2 = '/media/functionalspinelab/RAID/Data/Dinal/Pycharm_Data_ROI_by_slice/all_ROI_by_slice_2_data.csv'
+slice2_data = pd.read_csv(url2)
+
+url3 = '/media/functionalspinelab/RAID/Data/Dinal/Pycharm_Data_ROI_by_slice/all_ROI_by_slice_3_data.csv'
+slice3_data = pd.read_csv(url3)
+
+url4 = '/media/functionalspinelab/RAID/Data/Dinal/Pycharm_Data_ROI_by_slice/all_ROI_by_slice_4_data.csv'
+slice4_data = pd.read_csv(url4)
+
+dhi_features = ["DTI ADC", "DTI Axial", "DTI FA", "DTI Radial", "Fiber Axial", "Fiber FA ",
+    "Fiber Radial", "Fiber Fraction", "Hindered Fraction", "Restricted Fraction", "Water Fraction", "Axon Volume", "Inflammation Volume"]
+
+for i in range(len(dhi_features)):
+    filter_data1 = slice1_data[slice1_data['feature_col'] == dhi_features[i]]
+    filter_data2 = slice2_data[slice2_data['feature_col'] == dhi_features[i]]
+    filter_data3 = slice3_data[slice3_data['feature_col'] == dhi_features[i]]
+    filter_data4 = slice4_data[slice4_data['feature_col'] == dhi_features[i]]
+
+    # create grouped boxplot
+    plt.figure()
+    fig, axes = plt.subplots(2, 2, sharex=True, sharey= True,figsize=(12, 16))
+    fig.suptitle(dhi_features[i])
+
+    #slice 1
+    sns.boxplot(ax = axes[0,0], x = filter_data1['group'],
+                     y = filter_data1['data'],
+                     hue = filter_data1['operation'])
+    axes[0,0].set_title('Slice 1 - C6')
+    axes[0,0].set_ylabel("")
+
+    #slice 2
+    sns.boxplot(ax = axes[0,1], x = filter_data2['group'],
+                     y = filter_data2['data'],
+                     hue = filter_data2['operation'])
+    axes[0,1].set_title('Slice 2 - C5')
+    axes[0,1].set_ylabel("")
+
+    #slice 3
+    sns.boxplot(ax = axes[1,0], x = filter_data3['group'],
+                     y = filter_data3['data'],
+                     hue = filter_data3['operation'])
+    axes[1,0].set_title('Slice 3 - C4')
+    axes[1,0].set_ylabel("")
+
+    #slice 4
+    sns.boxplot(ax = axes[1,1], x = filter_data4['group'],
+                     y = filter_data4['data'],
+                     hue = filter_data4['operation'])
+    axes[1,1].set_title('Slice 4 - C3')
+    axes[1,1].set_ylabel("")
+    plt.show()
+
+
