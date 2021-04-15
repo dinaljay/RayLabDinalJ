a26b06b73f4e323eb43ebd60e516663de237057c
dinaljay <dinal.ais@gmail.com>
2021-04-15 15:23:21 -0500

    Updates

--- Python_scripts/Post_op/Plots/boxplot_post_op.py
+++ Python_scripts/Post_op/Plots/boxplot_post_op.py
@@ -8,21 +8,22 @@ import seaborn as sns
 
 ## Load Data
 
-url = '/media/functionalspinelab/RAID/Data/Dinal/Pycharm_Data/White_Matter/Pycharm_Data_improv_vs_nonimprov/all_ROI_data.csv'
+url = '/media/functionalspinelab/RAID/Data/Dinal/Pycharm_Data/White_Matter/Pycharm_Data_ROI/all_ROI_data.csv'
 
 all_data = pd.read_csv(url)
+
+post_op_data = all_data[all_data['operation']== 'Post_op']
 
 dhi_features = ["DTI ADC", "DTI Axial", "DTI FA", "DTI Radial", "Fiber Axial", "Fiber FA ",
     "Fiber Radial", "Fiber Fraction", "Hindered Fraction", "Restricted Fraction", "Water Fraction", "Axon Volume", "Inflammation Volume"]
 
 for i in range(len(dhi_features)):
-    filter_data = all_data[all_data['feature_col'] == dhi_features[i]]
+    filter_data = post_op_data[post_op_data['feature_col'] == dhi_features[i]]
 
     # create grouped boxplot
     plt.figure()
-    ax = sns.boxplot(x = filter_data['operation'],
-                     y = filter_data['data'],
-                     hue = filter_data['group'], showfliers=False)
+    ax = sns.boxplot(x = filter_data['group'],
+                     y = filter_data['data'], showfliers=False)
     ax.set_title(dhi_features[i])
     ax.set_ylabel("")
     ax.set_xlabel("")
